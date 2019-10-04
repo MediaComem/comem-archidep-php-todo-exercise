b32a0326582ecd97dc9137038e72220e413d3d45
Laurie Pouly <laurie.pouly@heig-vd.ch>
2019-10-04 16:57:41 +0200

    configuration infos de connexion à la BD

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-define('BASE_URL', '/');
+define('BASE_URL', '/comem-archidep-php-todo-exercise/'); // doit correspondre au demin de base: 
 define('DB_USER', 'todolist');
-define('DB_PASS', 'chAngeMeN0w!');
+define('DB_PASS', 'pouly');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_PORT', '8889');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
