e377accdfdf674eb750c50bc76604d9324aedc1a
Olivia Pradervand <olivia.pradervand@heig-vd.ch>
2019-10-07 14:59:43 +0200

    affichage des taches dans l'ordre decroissant de date de creation

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-define('BASE_URL', '/comem-archidep-php-todo-exercise/'); // doit correspondre au demin de base: 
-define('DB_USER', 'todolist');
-define('DB_PASS', 'pouly');
+define('BASE_URL', '../comem-archidep-php-todo-exercise/'); // doit correspondre au demin de base: 
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '8889');
@@ -71,7 +71,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM todo ORDER BY created_at DESC;'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
