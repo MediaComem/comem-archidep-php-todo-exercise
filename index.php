8b7c5ec00fad3083cb9a43220bf64617ff865860
Caroline Hache <hachecaroline@hotmail.com>
2022-10-11 12:38:54 +0200

    Implement delete button

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = ''; // IMPLEMENT ME
+        $deleteQuery = 'DELETE FROM todo WHERE todo.id = id'; // IMPLEMENT ME
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
