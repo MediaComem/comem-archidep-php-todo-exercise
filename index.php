562d6903707de61527a774ab2f652c44ed8da689
Laura DAddese <laura.daddese@heig-vd.ch>
2022-10-11 12:33:48 +0200

    Implement select * from todo

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/');
+define('BASE_URL', '/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT * from todo'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
