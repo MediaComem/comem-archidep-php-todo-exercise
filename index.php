6e0f8d379ccf8721686512563af72a8b114e5faa
Caroline Hache <hachecaroline@hotmail.com>
2022-10-11 12:45:53 +0200

    Implement toogle button

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = 'UPDATE todo SET done = NOT done'; // IMPLEMENT ME
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
