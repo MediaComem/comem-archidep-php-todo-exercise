9a3dbd165c8d38c1fe63fe021f070832065c1aa8
Simon Oulevay <simon.oulevay@gmail.com>
2024-09-29 02:54:46 +0200

    Update password in todolist.sql

--- todolist.sql
+++ todolist.sql
@@ -3,9 +3,9 @@ CREATE DATABASE IF NOT EXISTS todolist;
 -- The "WITH mysql_native_password" portion of this query is required for some
 -- older versions of MySQL. If you get a "syntax error [...] near BY" message,
 -- simply remove that part and use only "IDENTIFIED BY".
-CREATE USER IF NOT EXISTS 'todolist'@'localhost' IDENTIFIED WITH mysql_native_password BY 'chAngeMeN0w!';
+CREATE USER IF NOT EXISTS 'todolist'@'localhost' IDENTIFIED WITH mysql_native_password BY 'change-me-now';
 
-GRANT ALL PRIVILEGES ON todolist.* TO 'todolist'@'localhost' ;
+GRANT ALL PRIVILEGES ON todolist.* TO 'todolist'@'localhost';
 
 USE todolist;
 
